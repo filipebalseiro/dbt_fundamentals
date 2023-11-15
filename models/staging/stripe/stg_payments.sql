0e005ba60b54fb97c1b9fbeb57bb04b48f1e0cbc
Filipe Balseiro <filipe_balseiro@hakkoda.io>
2023-11-15 16:38:43 +0000

    update references to point to staging tables

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,6 +5,6 @@ select
     created as payment_created_at,
     status as payment_status,
     -- amount is stored in cents, convert it to dollars
-    round(amount/100.0, 2) as payment_amount
+    round(amount / 100.0, 2) as payment_amount
 
 from {{ source('stripe', 'payment') }}
