43ba830225f2ad0aeb8caccd69b51f6532ad343a
Filipe Balseiro <filipe_balseiro@hakkoda.io>
2023-11-15 16:22:10 +0000

    simplify with more window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -52,18 +52,6 @@ with
     left join customers on orders.user_id = customers.id 
     ),
 
-    customer_orders as (
-        select 
-            customers.id as customer_id,
-            min(orders.order_date) as first_order_date,
-            max(orders.order_date) as most_recent_order_date,
-            count(orders.id) as number_of_orders
-
-        from customers
-        left join orders on orders.user_id = customers.id 
-        group by 1
-    ),
-
     -- Final CTE
     final as (
         select
@@ -83,9 +71,15 @@ with
             row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
 
             -- new vs returning customer
-            case when customer_orders.first_order_date = paid_orders.order_placed_at
-            then 'new'
-            else 'return' end as nvsr,
+            case 
+                when (
+                rank() over (
+                    partition by customer_id
+                    order by order_placed_at, order_id
+                    ) = 1
+                ) then 'new'
+                else 'return'
+            end as nvsr,
 
             -- customer lifetime value
             sum(paid_orders.total_amount_paid) over (
@@ -94,11 +88,13 @@ with
                 ) as customer_lifetime_value,
 
             -- first day of sale
-            customer_orders.first_order_date as fdos
+            first_value(paid_orders.order_placed_at) over (
+                partition by paid_orders.customer_id
+                order by paid_orders.order_placed_at
+                ) as fdos
         from paid_orders
-        left join customer_orders using (customer_id)
-    order by order_id
     )
 
 -- Simple Select Statement
 select * from final
+order by order_id
