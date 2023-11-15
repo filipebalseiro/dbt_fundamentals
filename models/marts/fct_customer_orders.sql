2095acb5b5680daddc33986e57e6025ac8c9b26e
Filipe Balseiro <filipe_balseiro@hakkoda.io>
2023-11-15 15:53:22 +0000

    pull out intermediates and simple subqueries

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -34,7 +34,6 @@ with
             
     ),
 
-
     paid_orders as (
 
         select 
@@ -64,30 +63,33 @@ with
         left join orders
             on orders.user_id = customers.id 
         group by 1
+    ),
+
+    -- Final CTE
+    final as (
+        select
+            p.*,
+            row_number() over (order by p.order_id) as transaction_seq,
+            row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+            case when c.first_order_date = p.order_placed_at
+            then 'new'
+            else 'return' end as nvsr,
+            x.clv_bad as customer_lifetime_value,
+            c.first_order_date as fdos
+        from paid_orders p
+        left join customer_orders as c using (customer_id)
+        left join (
+            select
+            paid_orders.order_id,
+            sum(t2.total_amount_paid) as clv_bad
+        from paid_orders p
+        left join paid_orders t2 
+            on paid_orders.customer_id = t2.customer_id and paid_orders.order_id >= t2.order_id
+        group by 1
+        order by paid_orders.order_id
+        ) x on x.order_id = paid_orders.order_id
+    order by order_id
     )
 
-select
-    paid_orders.*,
-    row_number() over (order by paid_orders.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
-    case 
-        when customers.first_order_date = paid_orders.order_placed_at
-        then 'new'
-        else 'return' 
-    end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    customers.first_order_date as fdos
-from paid_orders
-left join customer_orders as c using (customer_id)
-left outer join 
-    (
-    select
-        paid_orders.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 
-        on paid_orders.customer_id = t2.customer_id and paid_orders.order_id >= t2.order_id
-    group by 1
-    order by paid_orders.order_id
-    ) x on x.order_id = paid_orders.order_id
-order by order_id
+-- Simple Select Statement
+select * from final
