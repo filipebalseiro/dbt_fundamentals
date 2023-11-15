553b8a8e18a1802ab059899dc1de89ed3999b74d
Filipe Balseiro <filipe_balseiro@hakkoda.io>
2023-11-15 14:35:50 +0000

    Updated single aliases to fully qualified table names

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -42,52 +42,52 @@ with
             orders.user_id	as customer_id,
             orders.order_date as order_placed_at,
             orders.status as order_status,
-            p.total_amount_paid,
-            p.payment_finalized_date,
-            c.first_name    as customer_first_name,
-            c.last_name as customer_last_name
+            completed_payments.total_amount_paid,
+            completed_payments.payment_finalized_date,
+            customers.first_name    as customer_first_name,
+            customers.last_name as customer_last_name
 
         from orders as orders
-        left join completed_payments as p on orders.id = p.order_id
+        left join completed_payments on orders.id = completed_payments.order_id
 
-    left join customers c on orders.user_id = c.id 
+    left join customers on orders.user_id = customers.id 
     ),
 
     customer_orders as (
         select 
-            c.id as customer_id
-            , min(order_date) as first_order_date
-            , max(order_date) as most_recent_order_date
+            customers.id as customer_id
+            , min(orders.order_date) as first_order_date
+            , max(orders.order_date) as most_recent_order_date
             , count(orders.id) as number_of_orders
 
-        from customers c 
+        from customers
         left join orders
-            on orders.user_id = c.id 
+            on orders.user_id = customers.id 
         group by 1
     )
 
 select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+    paid_orders.*,
+    row_number() over (order by paid_orders.order_id) as transaction_seq,
+    row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
     case 
-        when c.first_order_date = p.order_placed_at
+        when customers.first_order_date = paid_orders.order_placed_at
         then 'new'
         else 'return' 
     end as nvsr,
     x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
+    customers.first_order_date as fdos
+from paid_orders
 left join customer_orders as c using (customer_id)
 left outer join 
     (
     select
-        p.order_id,
+        paid_orders.order_id,
         sum(t2.total_amount_paid) as clv_bad
     from paid_orders p
     left join paid_orders t2 
-        on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+        on paid_orders.customer_id = t2.customer_id and paid_orders.order_id >= t2.order_id
     group by 1
-    order by p.order_id
-    ) x on x.order_id = p.order_id
+    order by paid_orders.order_id
+    ) x on x.order_id = paid_orders.order_id
 order by order_id
