4f74240a5853256ed33f83289b4db400906d58d3
Filipe Balseiro <filipe_balseiro@hakkoda.io>
2023-11-17 14:40:00 +0000

    Solved issues on daily run. comma missing on select statement

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -2,8 +2,7 @@ select
     id as order_id,
     user_id as customer_id,
     order_date as order_placed_at,
-    status as order_status
-
+    status as order_status,
     case 
         when status not in ('returned','return_pending') 
         then order_date 
